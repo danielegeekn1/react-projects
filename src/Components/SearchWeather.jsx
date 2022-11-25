a3bf5c746fe17184b989d3ee7adc3867a5f86fa0
daniele basile <danielebasile@danieles-MacBook-Air.local>
2022-11-25 10:06:56 +0100

    add components and card with bootstrap

---
+++ src/Components/SearchWeather.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const SearchWeather = () => {
+  return (
+    <div>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4">
+            <div class="card bg-dark text-white">
+              <img class="card-img" src="..." alt="..." />
+              <div class="card-img-overlay">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">
+                  This is a wider card with supporting text below as a natural
+                  lead-in to additional content. This content is a little bit
+                  longer.
+                </p>
+                <p class="card-text">Last updated 3 mins ago</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SearchWeather;
